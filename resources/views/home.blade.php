5cfda0efd6b9551715bde3c6926b853187da03cd
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-11 16:45:33 +0200

    add film image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,7 @@
             <h1>I film del momento</h1>
                 @foreach ($movies as $movie)
                     <div>
+                        <img style="height: 300px;" src="{{asset($movie['image'])}}" alt="{{$movie['title']}}">
                         <h2>Titolo: {{$movie['title']}}</h2>
                         <h4>Titolo originale: {{$movie['original_title']}}</h4>
                         <span>Nazionalità: {{$movie['nationality']}}</span> <br>
