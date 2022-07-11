0b05a13836abe4ea54b54a61c65899d00a1559c9
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-11 13:02:44 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.base')
+
+@section('title')
+    Movies
+@endsection
+
+@section('page-content')
+    <main>
+        <div class="container">
+            
+        </div>
+    </main>
+@endsection
