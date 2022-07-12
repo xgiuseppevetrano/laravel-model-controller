a6669282e97a4d3bf14141d3c8453f33f7ddde0d
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-07-12 09:39:55 +0200

    refactoring

--- resources/views/partials/movie.blade.php
+++ resources/views/partials/movie.blade.php
@@ -8,14 +8,14 @@
     <main>
         <div class="container">
             <h1>Film selezionato</h1>
-                <div>
-                    <img style="height: 300px;" src="{{asset($movie['image'])}}" alt="{{$movie['title']}}">
-                    <h2>Titolo: {{$movie['title']}}</h2>
-                    <h4>Titolo originale: {{$movie['original_title']}}</h4>
-                    <span>Nazionalità: {{$movie['nationality']}}</span> <br>
-                    <span>Data uscita: {{$movie['date']}}</span> <br>
-                    <span>Voto: {{$movie['vote']}}</span>
-                </div>
+            <div>
+                <img style="height: 300px;" src="{{asset($movie['image'])}}" alt="{{$movie['title']}}">
+                <h2>Titolo: {{$movie['title']}}</h2>
+                <h4>Titolo originale: {{$movie['original_title']}}</h4>
+                <span>Nazionalità: {{$movie['nationality']}}</span> <br>
+                <span>Data uscita: {{$movie['date']}}</span> <br>
+                <span>Voto: {{$movie['vote']}}</span>
+            </div>
         </div>
     </main>
 @endsection
